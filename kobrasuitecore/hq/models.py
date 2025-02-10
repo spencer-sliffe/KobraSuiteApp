3a5ddc5be370aa9b6702a9db3d5a07e7aeb656d4
jakeb <jakebernard@ku.edu>
2025-02-10 17:18:52 -0600

    Added model outlines for HQ

--- kobrasuitecore/hq/models.py
+++ kobrasuitecore/hq/models.py
@@ -23,6 +23,15 @@ class UserProfile(models.Model):
     )
     preferences = models.JSONField(null=True, blank=True)
 
+    def experience(self):
+        """ return a sum of all of a user's ModuleExperience """
+
+    def multiplier(self):
+        """ may be used to return a user's multiplier """
+
+    def get_status(self):
+        """ returns max streak for each module as a dictionary from each module's ModuleStreak """
+
     def __str__(self):
         return f"Profile of {self.user.username}"
 
@@ -145,3 +154,44 @@ class Multiplier(models.Model):
 
     def __str__(self):
         return f"Multiplier for {self.profile.user.username}"
+
+class Wallet(models.Model):
+    profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name='wallet'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    balance = models.IntegerField(default=0)
+
+class ModuleExperience(models.Model):
+    """
+    Each ModuleExperience object should be related to a module profile and a user profile.
+    This is a one-to-one relationship. There is an ModuleExperience object per module per user profile.
+    There are 4 ModuleExperience objects per user profile. These profiles are School, Finance, HomeLife, and Work.
+
+    There is a field for the amount of experience, which should be a double.
+    """
+
+class ModuleStatus(models.Model):
+    """
+    Per user, per module, ModuleStatus has a one-to-one relationship.
+    Each user, for each module, has a ModuleStatus object.
+    The module status tracks the login streak of the user per module and also keeps track of their longest login streak.
+    The start of the latest streak is also kept as a date.
+    """
+    streak_start = models.DateField(null=True, blank=True)
+    current_streak = models.IntegerField(default=0)
+    max_streak = models.IntegerField(default=0)
+
+class ModulePopulation(models.Model):
+    """
+    Per user, per module, ModulePopulation has a one-to-one relationship.
+    Each user, for each module, has a ModulePopulation object.
+    The ModulePopulation tracks a population field only, which is an integer.
+    The population field is
+    """
+    population = models.IntegerField(default=0)
+
+
--- kobrasuitecore/hq/types.py
+++ kobrasuitecore/hq/types.py
@@ -1,1 +1,5 @@
 # Just putting this file here for organization
+
+"""
+    Need a module type (finance, school, homelife)
+"""
