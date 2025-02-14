b3a0a60b9171744a72c835f8ee8dc88cface7359
Spencer Sliffe <spenceronw@ku.edu>
2025-02-14 15:43:07 -0600

    Signal in HQ had an import error

--- kobrasuitecore/hq/signals.py
+++ kobrasuitecore/hq/signals.py
@@ -1,6 +1,8 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from homelife.models import ChoreCompletion
+from hq.models import ModuleExperience, Wallet
+
 
 @receiver(post_save, sender=ChoreCompletion)
 def update_experience_on_chore_completion(sender, instance, created, **kwargs):
@@ -9,6 +11,7 @@ def update_experience_on_chore_completion(sender, instance, created, **kwargs):
         experience, _ = ModuleExperience.objects.get_or_create(profile=user_profile, module_type='HOMELIFE')
         experience.experience_amount += instance.points
         experience.save()
+
 
 @receiver(post_save, sender=ChoreCompletion)
 def update_wallet_on_chore_completion(sender, instance, created, **kwargs):
