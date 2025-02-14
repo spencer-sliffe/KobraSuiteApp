35f2161584b4572732f1293b1db992d3cf634634
jakeb <jakebernard@ku.edu>
2025-02-14 13:42:50 -0600

    models outline for exp, multiplier, wallet, etc

--- kobrasuitecore/hq/models.py
+++ kobrasuitecore/hq/models.py
@@ -120,7 +120,7 @@ class ModuleExperience(models.Model):
 class ModuleStatus(models.Model):
     profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='module_statuses')
     module_type = models.CharField(max_length=20, choices=ModuleType.choices)
-    streak_start = models.DateField(null=True, blank=True)
+    streak_start = models.DateField(null=True, gblank=True)
     current_streak = models.IntegerField(default=0)
     max_streak = models.IntegerField(default=0)
     building_level = models.IntegerField(default=0)
