0cabf999c994b79ec3176f96322c0456ca1cc137
Spencer Sliffe <spenceronw@ku.edu>
2025-02-19 20:14:31 -0600

    Revert "Merge pull request #9 from spencer-sliffe/Chucks_Branch(Budgeting)"
    This reverts commit 3fc3f6200f7ac14d1f3ea67421168bd5444e215b, reversing
    changes made to a7acbabeeb408c4395961c5e08cd57983f719fed.

--- kobrasuitecore/notifications/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import viewsets
-from .models import Notification
-
-class NotificationViewSet(viewsets.ModelViewSet):
-    queryset = Notification.objects.all()
-    # serializer_class = NotificationSerializer
