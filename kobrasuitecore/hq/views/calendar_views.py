324be60d7de7cffc8f1f75f246b105a52f0b524f
Spencer Sliffe <spenceronw@ku.edu>
2025-02-24 09:00:15 -0600

    KS-220 HQ Calander BE Bones
    Added Calander Event model, serializer, and viewset

--- kobrasuitecore/hq/views/calendar_views.py
+++ kobrasuitecore/hq/views/calendar_views.py
@@ -1,1 +1,17 @@
-# hq/serializers/calendar_views.py
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from hq.models import CalendarEvent
+from hq.serializers.calendar_serializers import CalendarEventSerializer
+
+
+class CalendarEventViewSet(ModelViewSet):
+    queryset = CalendarEvent.objects.all()
+    serializer_class = CalendarEventSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user = self.request.user
+        if not user.is_authenticated:
+            return qs.none()
+        return qs.filter(profile__user=user)
