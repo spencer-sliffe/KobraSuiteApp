2df521abfb16216ca876c61e708158ef8ca40771
Spencer Sliffe <spenceronw@ku.edu>
2025-03-08 13:21:00 -0600

    update to hq

--- kobrasuitecore/hq/views/module_task_views.py
+++ kobrasuitecore/hq/views/module_task_views.py
@@ -1,14 +1,14 @@
-
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from customer.permissions import IsOwnerOrAdmin
 from hq.models import ModuleTask
 from hq.serializers.module_task_serializers import ModuleTaskSerializer
 from hq.utils.task_utils import apply_task_rewards
+
 
 class ModuleTaskViewSet(viewsets.ModelViewSet):
     queryset = ModuleTask.objects.all()
@@ -16,12 +16,20 @@ class ModuleTaskViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrAdmin)
 
     @action(methods=['post'], detail=True)
-    def complete(self, request):
+    def complete(self, request, pk=None):
         serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         task = serializer.save()
-        currency, experience, population = apply_task_rewards(task)
-        return Response({"currency": currency, "experience": experience, "population": population})
-
+        currency, experience, population, task_completed, has_reward, performance, has_ranked_up = apply_task_rewards(task)
+        return Response({
+            "task_completed": task_completed,
+            "has_reward": has_reward,
+            "performance": performance,
+            "currency": currency,
+            "experience": experience,
+            "population": population,
+            "has_ranked_up": has_ranked_up
+        }, status=status.HTTP_200_OK)
 
     def get_queryset(self):
         user_pk = self.kwargs.get('user_pk')
@@ -31,7 +39,7 @@ class ModuleTaskViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         user_pk = self.kwargs.get('user_pk')
         module_type = self.kwargs.get('module_type')
-        serializer.save(profile=user_pk, module=module_type)
+        serializer.save(profile_id=user_pk, module=module_type)
 
     def perform_update(self, serializer):
         serializer.save(profile=self.get_object().profile, module=self.get_object().module)
